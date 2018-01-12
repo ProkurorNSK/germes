208c79c87e7696a777bdc2919ef502934a62ce6f
ProkurorNSK <proka@ngs.ru>
2018-01-12 19:07:17 +0700

    hibernate 0.3.4

--- germes-service/src/main/java/ru/prokurornsk/germes/app/config/ComponentBinder.java
+++ germes-service/src/main/java/ru/prokurornsk/germes/app/config/ComponentBinder.java
@@ -3,6 +3,7 @@ package ru.prokurornsk.germes.app.config;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import ru.prokurornsk.germes.app.persistence.hibernate.SessionFactoryBuilder;
 import ru.prokurornsk.germes.app.persistence.repository.CityRepository;
+import ru.prokurornsk.germes.app.persistence.repository.hibernate.HibernateCityRepository;
 import ru.prokurornsk.germes.app.persistence.repository.inmemory.InMemoryCityRepository;
 import ru.prokurornsk.germes.app.service.GeographicService;
 import ru.prokurornsk.germes.app.service.impl.GeographicServiceImpl;
@@ -19,7 +20,7 @@ import javax.inject.Singleton;
 public class ComponentBinder extends AbstractBinder {
     @Override
     protected void configure() {
-        bind(InMemoryCityRepository.class).to(CityRepository.class).in(Singleton.class);
+        bind(HibernateCityRepository.class).to(CityRepository.class).in(Singleton.class);
         bind(SimpleDTOTransformer.class).to(Transformer.class).in(Singleton.class);
         bind(GeographicServiceImpl.class).to(GeographicService.class).in(Singleton.class);
         bind(SessionFactoryBuilder.class).to(SessionFactoryBuilder.class).in(Singleton.class);
