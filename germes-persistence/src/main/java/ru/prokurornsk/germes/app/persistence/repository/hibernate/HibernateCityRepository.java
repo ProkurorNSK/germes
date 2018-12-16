5e5ddcbfbd4c48a350f0814924a7ee08f7404b76
ProkurorNSK <proka@ngs.ru>
2018-12-16 16:02:15 +0700

    0.3.4 Hibernate Repository

--- germes-persistence/src/main/java/ru/prokurornsk/germes/app/persistence/repository/hibernate/HibernateCityRepository.java
+++ germes-persistence/src/main/java/ru/prokurornsk/germes/app/persistence/repository/hibernate/HibernateCityRepository.java
@@ -1,15 +1,50 @@
 package ru.prokurornsk.germes.app.persistence.repository.hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import ru.prokurornsk.germes.app.model.entity.geography.City;
 import ru.prokurornsk.germes.app.persistence.hibernate.SessionFactoryBuilder;
+import ru.prokurornsk.germes.app.persistence.repository.CityRepository;
 
 import javax.inject.Inject;
+import java.util.List;
 
-public class HibernateCityRepository {
+public class HibernateCityRepository  implements CityRepository {
     private final SessionFactory sessionFactory;
 
     @Inject
     public HibernateCityRepository(SessionFactoryBuilder builder) {
         sessionFactory = builder.getSessionFactory();
+    }
+
+    @Override
+    public void save(City city) {
+        try (Session session = sessionFactory.openSession()) {
+            session.saveOrUpdate(city);
+        }
+    }
+
+    @Override
+    public City findById(int cityId) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(City.class, cityId);
+        }
+    }
+
+    @Override
+    public void delete(int cityId) {
+        try (Session session = sessionFactory.openSession()) {
+            City city = session.get(City.class, cityId);
+            if (city != null) {
+                session.delete(city);
+            }
+        }
+    }
+
+    @Override
+    public List<City> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createCriteria(City.class).list();
+        }
     }
 }
