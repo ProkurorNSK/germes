dbe21b4767e4c3eab80856d02c02d294b1729672
ProkurorNSK <proka@ngs.ru>
2019-11-01 13:10:11 +0700

    0.8.7 Locale

---
+++ germes-admin/src/main/java/ru/prokurornsk/germes/app/presentation/admin/bean/i18n/LanguageBean.java
@@ -0,0 +1,37 @@
+package ru.prokurornsk.germes.app.presentation.admin.bean.i18n;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.util.Locale;
+
+/**
+ * Keeps selected langauge for the current user
+ * @author ProkurorNSK
+ *
+ */
+
+@ManagedBean(name = "language")
+@SessionScoped
+public class LanguageBean {
+    /**
+     * Locale for the current user
+     */
+    private Locale locale;
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String lang) {
+        locale = new Locale(lang);
+    }
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
+
+}
+
